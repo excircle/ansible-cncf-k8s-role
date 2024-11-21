975c0c2750397ac43ba3eca72ad925eded144dc3
excircle <unixdisciple.us@gmail.com>
2024-11-21 10:14:19 -0800

    GHA Update

---
+++ kubeadm-join.sh
@@ -0,0 +1,2 @@
+kubeadm join k8scp:6443 --token dy0qlo.so62nt3rrzwjigsz \
+	--discovery-token-ca-cert-hash sha256:8fdb11521000e080efe6b63334c1c5fffe18273b882090702217418912603f46 --v=5 --ignore-preflight-errors=all
